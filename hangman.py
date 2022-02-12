8466f7bf8f03f81893e41d2be1a30bf166396d1c
Karthik Udyawar <karthikajitudy@gmail.com>
2022-02-13 02:28:03 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,90 +1,186 @@
 import random
 
 dataFile = "passwords.txt"
-def getData():
- 	lib = []
- 	f = open(dataFile, "r")
- 	for line in f:
- 		lib.append(line)
- 	f.close()
- 	print("# of Lines " + str(len(lib)))
- 	return lib
-
-def displayScore():
-	print("\n\t\tSCORE:\n\tUSER-> " + str(scoreUser) + "\tIDIOTS-> " + str(scoreCom) + "\n\n")	
-
-def getRandomWord():
-	rank = random.randint(0, 999) 
-	word = library[rank]
-	#bc you know there isn't a #0 most used password
-	rank += 1
-	return (rank, word)
-def getNextGuess():
-	val = list(input("Enter your next guess :\t>"))
-	if len(val) == 0:
-		val = getNextGuess()
-	return val[0]
-
-def drawHangman(remaining):
-	initialLimbs = 7
-	limbsToDraw = initialLimbs - remaining
-	print("     ________")
-	print("     |     |")
-	
-	print("     |     O" if limbsToDraw > 0 else "     |")
-	print("     |    /|\\" if limbsToDraw > 3 else "     |    /|" if limbsToDraw > 2 else "     |     |" if limbsToDraw > 1 else "     |")
-	print("     |     |" if limbsToDraw > 4 else "     |")
-	print("     |    / \\" if limbsToDraw > 6 else "     |    /" if limbsToDraw > 5 else "     |")
-	print(" ____|___")
-	
-
-def runGame():
-	(rank, word) = getRandomWord()
-	word = list(word)
-
-	guessed = word.copy()
-
-	roundsLeft = 7
-	for i in range(0,len(guessed)-1):
-		guessed[i] = '_'
-
-	while roundsLeft >= 0 :
-		containedFlag = 0
-		#print hangman
-		drawHangman(roundsLeft)
-		print(' '.join(guessed))
-		#ensure game is not over
-		if word == guessed:
-			print("Congraulatons! You have guessed the #" + str(rank) + " most used password " + ''.join(guessed));
-			return True
-		val = getNextGuess()
-		for i in range(0, len(guessed) -1):
-			if word[i] is val:
-				guessed[i] = word[i]
-				containedFlag += 1
-
-		if containedFlag == 0 :
-			roundsLeft -= 1
-		elif containedFlag >= 3:
-			print("Woah.. "+ str(containedFlag) + " " + str(val)+ "'s found.. take an extra limb")
-			roundsLeft += 1
-	
-	print("Embaressing! You have could not guess the #" + str(rank) + " most used password " + ''.join(word));
-	return False
 
 
-library = getData()
-print("\n\t\tWelcome to Hangman!\n\t Now With the top 1,000 passwords!\n\n")
-scoreUser = 0
-scoreCom = 0
-inp = "yeet"
-while inp[0] == 'y':
-	inp = input("Press 'y' to start new game or 'q' to quit...\n")
-	if inp[0] == 'y':
-		if runGame():
-			scoreUser += 1;
-		else:
-			scoreCom += 1;
-	displayScore()
+def getData() -> list:
+    """Fetches the list of passwords from txt file and returns the list
+
+    Returns:
+            list: List of passwords
+    """
+    lib = []
+    with open(dataFile, "r") as f:
+        lib.extend(iter(f))
+    print("# of Lines " + str(len(lib)))
+    return lib
 
 
+def displayScore() -> None:
+    """Prints score of user and computer
+
+    Returns:
+            None
+    """
+
+    print(
+        "\n\t\tSCORE:\n\tUSER-> "
+        + str(scoreUser)
+        + "\tIDIOTS-> "
+        + str(scoreCom)
+        + "\n\n"
+    )
+    return None
+
+
+def getRandomWord() -> "tuple[int, str]":
+    """Select random word from the list
+
+    Returns:
+            tuple[int, str]: Tuple of rank of the most common password with its passord
+    """
+    rank = random.randint(0, 999)
+    word = library[rank]
+    return (rank + 1, word)
+
+
+def getNextGuess() -> str:
+    """Gets the next guess of the user
+
+    Return:
+            First word of the user input guess
+    """
+
+    try:
+        val = list(input("Enter your next guess :\t>"))
+        if not val:
+            val = getNextGuess()
+        return val[0]
+    except (KeyboardInterrupt, EOFError):
+        exit()
+
+
+"""
+Hangman sprite 
+	________
+	|     |
+	|     O
+	|    /|\
+	|     |
+	|    / \
+____|___
+_ _ _ _ _ _ _ _ _
+"""
+
+
+def drawHangman(remaining: int) -> None:
+    """Drawing the Hangman
+
+    Args:
+            remaining (int): Remaining guess of the user
+
+    Returns:
+            None
+    """
+    initialLimbs = 7
+    limbsToDraw = initialLimbs - remaining
+    print("     ________")
+    print("     |     |")
+
+    print("     |     O" if limbsToDraw > 0 else "     |")
+    print(
+        "     |    /|\\"
+        if limbsToDraw > 3
+        else "     |    /|"
+        if limbsToDraw > 2
+        else "     |     |"
+        if limbsToDraw > 1
+        else "     |"
+    )
+    print("     |     |" if limbsToDraw > 4 else "     |")
+    print(
+        "     |    / \\"
+        if limbsToDraw > 6
+        else "     |    /"
+        if limbsToDraw > 5
+        else "     |"
+    )
+    print(" ____|___")
+    return None
+
+
+def runGame() -> bool:
+    """Core function of the game
+
+    Returns:
+            bool: Result of the game
+    """
+    (rank, word) = getRandomWord()
+    word = list(word)
+
+    guessed = word.copy()
+
+    roundsLeft = 7
+    for i in range(len(guessed) - 1):
+        guessed[i] = "_"
+
+    while roundsLeft >= 0:
+        containedFlag = 0
+        # print hangman
+        drawHangman(roundsLeft)
+        print(" ".join(guessed))
+        # ensure game is not over
+        if word == guessed:
+            print(
+                "Congratulations! You have guessed the #"
+                + str(rank)
+                + " most used password "
+                + "".join(guessed)
+            )
+            return True
+        val = getNextGuess()
+        for i in range(len(guessed) - 1):
+            if word[i] is val:
+                guessed[i] = word[i]
+                containedFlag += 1
+
+        if containedFlag == 0:
+            roundsLeft -= 1
+        elif containedFlag >= 3:
+            print(
+                "Whoa.. "
+                + str(containedFlag)
+                + " "
+                + str(val)
+                + "'s found.. take an extra limb"
+            )
+            roundsLeft += 1
+
+    print(
+        "Embaressing! You have could not guess the #"
+        + str(rank)
+        + " most used password "
+        + "".join(word)
+    )
+    return False
+
+
+# Main Loop
+if __name__ == "__main__":
+    library = getData()
+    print("\n\t\tWelcome to Hangman!\n\t Now With the top 1,000 passwords!\n\n")
+    scoreUser, scoreCom = 0, 0
+    running = True
+    while running:
+        try:
+            inp = str(input("Press 'y' to start new game or 'q' to quit...\n")).lower()
+        except (KeyboardInterrupt, EOFError):
+            exit()
+        if inp != "y":
+            running = False
+        else:
+            if runGame():
+                scoreUser += 1
+            else:
+                scoreCom += 1
+            displayScore()
